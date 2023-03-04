fa5bd65692966ac517d4d05f1cfa074a436bb365
jim codeman <akomikron@gmail.com>
2023-03-04 08:55:57 +0200

    updated config file.

--- source/loomlib/loomlib_config.h
+++ source/loomlib/loomlib_config.h
@@ -2,12 +2,8 @@
 
 #ifndef NDEBUG
 #  define DEBUG_NAME_LEN 64
-#else
-#  define DEBUG_NAME_LEN 32
-#endif
-
-#ifndef NDEBUG
 #  define DEFAULT_STRING_LEN 127
 #else
+#  define DEBUG_NAME_LEN 32
 #  define DEFAULT_STRING_LEN 255
 #endif
